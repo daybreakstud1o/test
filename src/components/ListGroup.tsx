31fcb368ca5b6bdd144131ff21e57094f2d1ed28
kiranpate1 <kp8568@gmail.com>
2024-03-14 13:09:30 -0400

    test

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,31 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
-  items = [];
+  let selectedIndex = 0;
+
+  const handleClick = (event: MouseEvent) => console.log(event.clientX);
+  function handleTest(test: string, event: MouseEvent, index: number) {
+    console.log(test, event.clientX);
+    const className =
+      selectedIndex === index ? "list-group-item active" : "list-group-item";
+  }
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onMouseOver={(event) => handleTest(item, event, index)} // Fix: Pass both item, event, and index parameters to handleTest
+          >
             {item}
           </li>
         ))}
